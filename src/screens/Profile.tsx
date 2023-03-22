acf8164e13727375c675d6d157363738555d94e4
Nickfln <nke1@live.com>
2023-03-22 11:52:57 -0300

    Profile Icon changes

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -5,11 +5,16 @@ import { UserPhoto } from '@components/UserPhoto'
 import { TouchableOpacity } from 'react-native';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
+import * as ImagePicker from 'expo-image-picker'
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false)
+
+  async function handleUserPhotoSelect(){
+    await ImagePicker.launchImageLibraryAsync()
+  }
 
   return (
     <VStack flex={1}>
@@ -34,7 +39,7 @@ export function Profile() {
 
           }
 
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text color={"green.500"} fontWeight="bold" mt={2} mb={8}>
               Alterar foto
             </Text>
