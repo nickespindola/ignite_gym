a85aa0053404a532627680aa4a91aaa62f44b46b
Nickfln <nke1@live.com>
2023-03-28 18:14:13 -0300

    sign up form updates

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,20 +1,33 @@
+import React from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import { useForm, Controller } from 'react-hook-form'
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 
 import { Input } from '@components/Input'
 import { Button } from '@components/Button'
-import React from 'react'
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
 
 export function SignUp() {
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>()
 
   const navigation = useNavigation()
 
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
+    console.log({ name, email, password, password_confirm });
+
   }
 
   return (
@@ -40,30 +53,87 @@ export function SignUp() {
           <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
             Crie sua conta
           </Heading>
+
+          <Controller
+            control={control}
+            name="name"
+            rules={{
+              required: 'Informe o nome.'
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+                isInvalid
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: 'Informe o e-mail.',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: 'E-mail inválido'
+              }
+            }}
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password_confirm"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirme a senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+              />
+            )}
+          />
+
+          <Button
+            title="Criar e Acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
+
+          <Button
+            title="Voltar para o login"
+            variant={'outline'}
+            mt={24}
+            onPress={handleGoBack}
+          />
+
         </Center>
-
-        <Input
-          placeholder="Nome"
-        />
-        <Input
-          placeholder="E-mail"
-          keyboardType="email-address"
-          autoCapitalize="none"
-        />
-        <Input
-          placeholder="Senha"
-          secureTextEntry
-        />
-
-        <Button title="Criar e Acessar" />
-
-        <Button
-          title="Voltar para o login"
-          variant={'outline'}
-          mt={24}
-          onPress={handleGoBack}
-        />
-
       </VStack>
     </ScrollView>
   )
