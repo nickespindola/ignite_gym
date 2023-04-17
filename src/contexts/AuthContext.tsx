ffaf688cafec740c4b1887dc4bfdb5b27117e3c0
Nickfln <nke1@live.com>
2023-04-17 10:30:52 -0300

    context state updates

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { UserDTO } from "@dtos/UserDTO";
 import { ReactNode, createContext } from "react";
 
 export type AuthContextDataProps = {
   user: UserDTO
+  signIn: (email: string, password: string) => void
 }
 
 type AuthContextProviderProps = {
@@ -13,15 +14,24 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps)
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [user, setUser] = useState({
+    id: '1',
+    name: 'Nicolas Espindola',
+    email: 'nicolas@email.com',
+    avatar: 'nicolas.png'
+  })
+
+  function signIn(email: string, password: string){
+    setUser({ 
+      id: '',
+      name: '',
+      email,
+      avatar: '',
+    });
+  }
+
   return (
-    <AuthContext.Provider value={{
-      user: {
-        id: '1',
-        name: 'Nicolas',
-        email: 'nicolas@email.com',
-        avatar: 'nicolas.png'
-      }
-    }}>
+    <AuthContext.Provider value={{ user, signIn }}>
       {children}
     </AuthContext.Provider>
   )
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 import { useNavigation } from "@react-navigation/native"
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
-import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 import { useForm, Controller } from "react-hook-form"
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
+
+import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
+
+import { useAuth } from '@hooks/useAuth'
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
@@ -23,6 +26,8 @@ const signInSchema = yup.object({
 })
 
 export function SignIn() {
+  const { signIn } = useAuth()
+
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signInSchema)
   })
@@ -34,8 +39,7 @@ export function SignIn() {
   }
 
   function handleSignIn({ email, password }: FormDataProps) {
-    console.log({ email, password });
-
+    signIn(email, password)
   }
 
   return (
